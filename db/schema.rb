1350b3ec6ec24733b58bffd1f7784de5e55a1853
tunanosuke <gakkun1020@gmail.com>
2022-02-08 13:36:37 +0000

    Book モデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_045826) do
+ActiveRecord::Schema.define(version: 2022_02_08_132950) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "opinion"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
