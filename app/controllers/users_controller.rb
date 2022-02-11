d950afc9bf1b23431a6494829a66577b4cd71236
tunanosuke <gakkun1020@gmail.com>
2022-02-11 16:50:02 +0000

    users テーブルの image を profile_image に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
