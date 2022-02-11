d56fc76b06a1e13ae420655d5235e7b417414bd4
tunanosuke <gakkun1020@gmail.com>
2022-02-11 15:14:20 +0000

    ユーザー情報の更新処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def set_user_book
     @user_book = Book.new
   end
+
+  def render_404
+    render :file => "/public/404.html", :status => 404
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,32 @@
 class UsersController < ApplicationController
+  before_action :check_current_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    user = current_user
+    user.update(user_params)
+    redirect_to user_path(user)
   end
 
   def show
-    @user_books = current_user.books
+    @user = User.find(params[:id])
+    @user_books = @user.books
+  end
+
+  private
+
+  def check_current_user
+    return render_404 unless User.find(params[:id]) == current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
